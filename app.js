c38e47a18e83d17877667507ad2d379161754b08
Jack Werner <jwerner@Jacks-MacBook-Pro-2.local>
2020-10-06 21:12:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const userScore = 0;
+const computerScore =0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+
+function getComputerChoice (){
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = userChoice+"beats"+computerChoice+ "YOU WIN"
+}
+function lose(){
+    
+}
+function draw(){
+    console.log("Draw");
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win (userChoice, computerChoice);
+            break;
+        case "sr":
+        case "rp":
+        case "ps":
+            lose (userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+             draw (userChoice, computerChoice); 
+             break;   
+    }
+
+
+}
+
+
+main();
+
+function main(){
+    rock_div.addEventListener('click', function(){
+        game("r");
+    })
+    paper_div.addEventListener('click', function(){
+        game("p");
+    })
+    scissor_div.addEventListener('click', function(){
+        game("s");
+    })
+}
+
